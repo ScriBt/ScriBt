0e21f22d6404ea115860021d87cd1edaa20c3438
a7r3 <arvindultimate7352@gmail.com>
2016-05-16 13:47:56 +0530

    Initial Colorizations! && Some corrections
    * Add a function to define the colors which are going to be used in this Script.
    * For now, only Definitions and a Color check has been added. Will think about and choose colors in various parts of the Script.
    
    * \n didn't work + Some Semicolons
    
    * Another Mistake

--- ROM.sh
+++ ROM.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-clear
-echo "Teh ROM Builder";
-echo "By Arvind7352 @XDA";
-sleep 3;
 #Initialize
 source $(pwd)/ROM.rc;
+echo "Before i can start, do you like a Colorful life? [y/n]";
+read COLOR;
+if [[ "$COLOR" == y ]]; then
+	color_my_life;
+fi
+clear;
+echo "Teh ROM Sync and Build Script";
+echo "By Arvind7352 @XDA";
+sleep 3;
 #GET THOSE ROOMS
 cat << _ROM_
+
+=======================================================
 Which ROM are you trying to build? Choose among these (Enter teh corresponding number for selection)
 
 1. AICP
@@ -25,6 +32,7 @@ Which ROM are you trying to build? Choose among these (Enter teh corresponding n
 14. XenonHD by Team Horizon
 15. Xperia Open Source Project aka XOSP
 
+=======================================================
 _ROM_
 echo "";
 echo "";
@@ -95,7 +103,7 @@ read DIR;
 mkdir $DIR;
 cd $DIR;
 
-echo "========================================================="
+echo "=========================================================";
 
 echo "Let's Initialize teh ROM Repo";
 repo init "$REF" -u https://github.com/"$ROM_NAME"/"$MAN" ${BRNC} ;
@@ -108,7 +116,7 @@ echo "Let's Sync!";
 repo sync -j${JOBS} ${SILENT} ${FRC} ;
 echo "DONE!";
 
-echo "========================================================="
+echo "=========================================================";
 
 echo "What's your Device's Good CodeName (Look at your Device tree and answer)?";
 read DEVICE;
@@ -122,7 +130,7 @@ rom_name_in_source;
 echo "Now, there are Four Strategies of Adding your device to the ROM vendor so that The ROM can get built for your device. Choose the file which you find in vendor/${ROMNIS}";
 echo "vendorsetup.sh (Enter 1)";
 echo "${ROMNIS}.devices (Enter 2)";
-echo "Synced AOSP-RRO / AOSP-CAF ? (Enter 3)"
+echo "Synced AOSP-RRO / AOSP-CAF ? (Enter 3)";
 echo "You see a folder named \'products\' inside teh folder (Enter 4)";
 read STRT;
 
@@ -140,20 +148,21 @@ fi
 if [[ "$STRT" == 3 ]]
 echo "Let's go to teh Device Directory!";
 cd $(pwd)/device/${COMP}/${DEVICE};
-echo "Need to create a vendorsetup.sh - I'll create that for you if it isn't"
+echo "Need to create a vendorsetup.sh - I'll create that for you if it isn't";
 if [[ $(-f vendorsetup.sh) != 1 ]]; then
 touch vendorsetup.sh;
 fi
 echo "Open that file and Enter the following contents";
 echo "";
-echo "add_lunch_combo ${ROMNIS}_${DEVICE}-${BTYP}"
+echo "add_lunch_combo ${ROMNIS}_${DEVICE}-${BTYP}";
 fi
 
 if [[ $STRT == 4 ]]; then
-	echo "This Strategy, AFAIK was only on AOKP (kitkat) and PAC-ROM (pac-5.1)."
+	echo "This Strategy, AFAIK was only on AOKP (kitkat) and PAC-ROM (pac-5.1).";
 	echo "Let's go to vendor/$ROMNIS/products";
-	cd vendor/${ROMNIS}/products
-	echo"\nDone."
+	cd vendor/${ROMNIS}/products;
+	echo "";
+	echo"Done.";
 if [[ "$ROMNIS" == pac]]; then
 	echo "Creating file ${ROMNIS}_${DEVICE}.mk";
 	touch ${ROMNIS}_${DEVICE}.mk
@@ -177,6 +186,6 @@ echo "========================================================="
 echo "Now start the build process! The Script's work is DONE. Enjoy!";
 echo "Thanks for using this script!";
 sleep 3;
-echo "Are you feeling even lazy to start the Build? OH, COME ON! -(these commands)- \n. build/envsetup.sh \nlunch ${ROMNIS}_${DEVICE}-${BTYP}";
+echo "Are you feeling even lazy to start the Build? OH, COME ON! -(these commands)- . build/envsetup.sh lunch ${ROMNIS}_${DEVICE}-${BTYP}";
 echo "";
 echo "I_IZ_NOOB :P";
