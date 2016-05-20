897b0414adc29c0132419c2e222200dde240cbf9
a7r3 <arvindultimate7352@gmail.com>
2016-05-20 12:44:14 +0530

    COLORIZE!
    * Splashed the Entire Script with some Colors! Don't worry Color Haters, you won't have it.
    * Some more Missing Semicolons

--- ROM.sh
+++ ROM.sh
@@ -53,25 +53,25 @@ echo -e "${BLU} \"Y88888P\"    \`\"Ybbd8\"'  88          88  88888888P\"     \"Y
 sleep 0.1;
 echo -e '\n';
 sleep 0.1;
-echo -e "${CYAN} ~#~#~#~#~#~#~#~#~#~#~ By Arvind7352 @XDA #~#~#~#~#~#~#~#~#~#~ ${NONE}";
+echo -e "${CYAN}~#~#~#~#~#~#~#~#~#~#~ By Arvind7352 @XDA #~#~#~#~#~#~#~#~#~#~${NONE}";
 sleep 3;
 #GET THOSE ROOMS
 echo -e '\n\n';
 
 function main_menu
 {
-	echo -e "=======================================================";
-	echo -e "===================[*] MAIN MENU [*]===================";
-	echo -e "=======================================================";
+	echo -e "${LRED}=======================================================${NONE}";
+	echo -e "${LRED}====================${NONE}${CYAN}[*]${NONE}${PURP}MAIN MENU${NONE}${CYAN}[*]${NONE}${LRED}====================${NONE}";
+	echo -e "${LRED}=======================================================${NONE}";
 	echo -e '\n';
 	echo -e "         Select the Action you want to perform         ";
 	echo -e '\n';
-	echo -e "1 .......................Sync........................ 1";
-	echo -e "2 .....................Pre-Build..................... 2";
-	echo -e "3 .......................Build....................... 3";
+	echo -e "${LBLU}1${NONE}${CYAN} .......................${NONE}${RED}Sync${NONE}${CYAN}........................${NONE} ${LBLU}1${NONE}";
+	echo -e "${LBLU}2${NONE}${CYAN} .....................${NONE}${YELO}Pre-Build${NONE}${CYAN}.....................${NONE} ${LBLU}2${NONE}";
+	echo -e "${LBLU}3${NONE}${CYAN} .......................${NONE}${GRN}Build${NONE}${CYAN}.......................${NONE} ${LBLU}3${NONE}";
 	echo -e '\n';
 	echo -e "4 .......................EXIT........................ 4";
-	echo -e "=======================================================";
+	echo -e "${LRED}=======================================================${NONE}";
 	echo -e '\n';
 	read ACTION;
 	teh_action;
@@ -115,38 +115,39 @@ ${BLANK}
 16.${LBLU} SlimRoms ${NONE}
 ${BLANK}
 ${LPURP}=======================================================${NONE}";
-	echo -e '\n\n';
+	echo -e '\n';
 	read ROMNO;
 	sleep 1;
 	#
 	rom_name_in_github;
 	#
+	echo -e '\n';
 	echo -e "You have chosen ${LCYAN}->${NONE} $ROM_FN";
 	sleep 1;
 	echo -e '\n';
-	echo "Since Branches may live or die at any moment, specify the Branch you're going to sync"
+	echo -e "Since Branches may live or die at any moment, ${LRED}Specify the Branch${NONE} you're going to sync"
 	read BRANCH;
 	echo -e '\n';
-	echo "Let's sync it!";
+	echo -e "Let's sync it!";
 	echo -e '\n';
-	echo "Number of Threads for Sync?";
+	echo -e "${LRED}Number of Threads${NONE} for Sync?";
 	echo -e '\n';
 	read JOBS;
 	echo -e '\n';
-	echo "Force Sync needed? [y/n]";
+	echo -e "${LRED}Force Sync${NONE} needed? ${LGRN}[y/n]${NONE}";
 	echo -e '\n';
 	read FRC;
 	echo -e '\n';
-	echo "Need some Silence in teh Terminal? [y/n]";
+	echo -e "Need some ${LRED}Silence${NONE} in teh Terminal? ${LGRN}[y/n]${NONE}";
 	echo -e '\n';
 	read SILENT;
 	echo -e '\n';
-	echo "Any Source you have already synced? If yes, then say YES and Press ENTER";
+	echo -e "Any ${LRED}Source you have already synced?${NONE} If yes, then say YES and Press ${LCYAN}ENTER${NONE}";
 	echo -e '\n';
 	read REFY;
 	echo -e '\n';
 	if [[ "$REFY" == YES ]]; then
-		echo "Provide me the Synced Source's Location from / ";
+		echo -e "Provide me the ${LRED}Synced Source's Location${NONE} from / ";
 		read REFER;
 	fi
 		echo -e '\n';
@@ -184,39 +185,39 @@ ${LPURP}=======================================================${NONE}";
 		fi
 
 	#Naming && Syncing
-	echo "Name your Build Directory";
+	echo -e "Name your Build Directory";
 	read DIR;
 	mkdir $DIR;
 	cd $DIR;
 
-	echo "=========================================================";
+	echo -e "${LBLU}=========================================================${NONE}";
 	echo -e '\n';
-	echo "Let's Initialize teh ROM Repo";
+	echo -e "Let's Initialize teh ROM Repo";
 	echo -e '\n';
 	repo init "$REF" -u https://github.com/"$ROM_NAME"/"$MAN" -b "$BRANCH" ;
 	echo -e '\n';
-	echo "Repo Init'ed";
+	echo -e "Repo Init'ed";
 	echo -e '\n';
-	echo "=========================================================";
+	echo -e "${LBLU}=========================================================${NONE}";
 	echo -e '\n';
-	echo "Now Create a \"local_manifests.xml\" file and add your Device, Kernel, Vendor and other Device-Specific Sources. Press \"ENTER\" After it's Done";
+	echo -e "Now Create a folder named local_manifests, create a \"local_manifest.xml\" file in it and add your Device, Kernel, Vendor and other Device-Specific Repo links. Press \"${LCYAN}ENTER${NONE}\" After it's ${LPURP}Done.${NONE}";
 	read ENTER;
 	echo -e '\n';
-	echo "Let's Sync!";
+	echo -e "Let's Sync!";
 	echo -e '\n';
 	repo sync -j${JOBS} ${SILENT} ${FRC} ;
 	echo -e '\n';
-	echo "DONE!";
-	echo "=========================================================";
+	echo -e "${LPURP}Done.${NONE}!";
+	echo -e "${LRED}=========================================================${NONE}";
 #Next ACTION to be Performed
 echo -e '\n\n';
-echo -e "=========================================================";
+echo -e "${LRED}=========================================================${NONE}";
 echo -e '\n';
 echo -e " 2. Pre Build";
-echo -e " 3. Build    ";
+echo -e " ${CYAN}3${NONE}. ${GRN}Build${NONE}    ";
 echo -e '\n';
 echo -e " 4. EXIT     ";
-echo -e "=========================================================";
+echo -e "${LRED}=========================================================${NONE}";
 read ACTION;
 teh_action;
 
@@ -227,98 +228,100 @@ function pre_build
 
 	echo -e "${CYAN}Initializing Build Environment${NONE}"
 	. build/envsetup.sh
-	echo "DONE."
+	echo -e "${LPURP}Done.${NONE}."
 	echo -e '\n\n';
-	echo -e "=======================DEVICE INFO======================="
-	echo "What's your Device's Good CodeName (Look at your Device tree and answer)?";
+	echo -e "${LCYAN}=======================DEVICE INFO=======================${NONE}";
+	echo -e "What's your ${LRED}Device's CodeName${NONE} ${LGRN}[Refer Device Tree - All Lowercases]${NONE}?";
 	echo -e '\n';
 	read DEVICE;
 	echo -e '\n';
-	echo "The Build type? (userdebug/user/eng)";
+	echo -e "The ${LRED}Build type${NONE}? ${LGRN}[userdebug/user/eng]${NONE}";
   echo -e '\n';
 	read BTYP;
 	echo -e '\n';
-	echo "Your Device's Company/Vendor (All Lowercases)?";
+	echo -e "Your ${LRED}Device's Company/Vendor${NONE} (All Lowercases)?";
 	echo -e '\n';
 	read COMP;
 	echo -e '\n';
-	echo "=========================================================";
+	echo -e "${LCYAN}=========================================================${NONE}";
 	rom_name_in_source;
 	echo -e '\n\n'
-	echo "=========================================================";
-	echo "Now, there are Four Strategies of Adding your device to the ROM vendor so that The ROM can get built for your device. Choose the file which you find in vendor/${ROMNIS}";
-	echo "vendorsetup.sh (Enter 1)";
-	echo "${ROMNIS}.devices (If saw that, ignore presence of vendorsetup.sh)(Enter 2)";
-	echo "Synced AOSP-RRO / AOSP-CAF ? (Enter 3)";
-	echo "You see a folder named 'products' inside teh folder (Enter 4)";
+	echo -e "${LPURP}=========================================================${NONE}";
+	echo -e "Now, there are Four Strategies of Adding your device to the ROM vendor so that The ROM can get built for your device. Choose the file which you find in vendor/${ROMNIS}";
+	echo -e "${BLU}vendorsetup.sh (${LCYAN}ENTER${NONE} 1)${NONE}";
+	echo -e "${ORANGE}${ROMNIS}.devices (If saw that, ignore presence of vendorsetup.sh)(${LCYAN}ENTER${NONE} 2)${NONE}";
+	echo -e "${PURP}Synced AOSP-RRO / AOSP-CAF ? (${LCYAN}ENTER${NONE} 3)${NONE}";
+	echo -e "${GREEN}You see a folder named 'products' inside teh folder (${LCYAN}ENTER${NONE} 4)${NONE}";
 	read STRT;
 	echo -e '\n';
 	if [[ $STRT == 1 ]]; then
-		echo "Add this line at teh end of vendorsetup.sh";
-		echo "add_lunch_combo ${ROMNIS}_${DEVICE}_${BTYP}";
+		echo -e "Add this line at teh end of ${LBLU}vendorsetup.sh${NONE}";
+		echo -e '\n';
+		echo -e "add_lunch_combo ${ROMNIS}_${DEVICE}_${BTYP}";
 	fi
 
 	if [[ $STRT == 2 ]]; then
-		echo "Open ${ROMNIS}.devices file";
-		echo "Insert this at the End of the File";
-		echo "${DEVICE}";
+		echo -e "Open ${ROMNIS}.devices file";
+		echo -e "Insert this at the End of the File";
+		echo -e '\n';
+		echo -e "${DEVICE}";
 	fi
 
 	if [[ "$STRT" == 3 ]]; then
-		echo "Let's go to teh Device Directory!";
+		echo -e "Let's go to teh ${LRED}Device Directory!${NONE}";
 		cd $(pwd)/device/${COMP}/${DEVICE};
-		echo "Need to create a vendorsetup.sh - I'll create that for you if it isn't";
+		echo -e "Need to create a vendorsetup.sh - I'll create that for you if it isn't";
 			if [[ $(-f vendorsetup.sh) != 1 ]]; then
 				touch vendorsetup.sh;
 			fi
-		echo "Open that file and Enter the following contents";
+		echo -e "Open that file and ${LCYAN}ENTER${NONE} the following contents";
 		echo -e '\n'
-		echo "add_lunch_combo ${ROMNIS}_${DEVICE}-${BTYP}";
-		echo "Done. Let's go back."
+		echo -e "add_lunch_combo ${ROMNIS}_${DEVICE}-${BTYP}";
+		echo -e "${LPURP}Done.${NONE}. Let's go back."
 		croot;
 	fi
 	if [[ $STRT == 4 ]]; then
-		echo "This Strategy, AFAIK was only on AOKP (kitkat) and PAC-ROM (pac-5.1).";
-		echo "Let's go to vendor/$ROMNIS/products";
+		echo -e "This Strategy, AFAIK was only on AOKP (kitkat) and PAC-ROM (pac-5.1).";
+		echo -e "Let's go to vendor/$ROMNIS/products";
 		cd vendor/${ROMNIS}/products;
 		echo -e '\n';
-		echo "Done.";
+		echo -e "${LPURP}Done.${NONE}.";
 			if [[ "$ROMNIS" == pac ]]; then
-				echo "Creating file ${ROMNIS}_${DEVICE}.mk";
+				echo -e "Creating file ${ROMNIS}_${DEVICE}.mk";
 				touch ${ROMNIS}_${DEVICE}.mk
 			else
-				echo "Creating file ${DEVICE}.mk";
+				echo -e "Creating file ${DEVICE}.mk";
 				touch ${DEVICE}.mk
 			fi
 	fi
-		echo "\nDone. Open that file now."
-		echo "\nAdd these lines";
+		echo -e "\n${LPURP}Done.${NONE}. Open that file now."
+		echo -e "\nAdd these lines";
 			if [[ "$ROMNIS" == pac ]]; then
-				echo "FAIL. WIP";
+				echo -e "FAIL. WIP";
 			fi
 			if [[ "$ROMNIS" == aokp ]]; then
 				${BLANK}
-				echo "WIP WIP!";
+				echo -e "WIP WIP!";
 			fi
 		croot;
 		echo -e '\n\n'
-		echo "Now ${ROMNIS}fy! your Device Tree! Press Enter when DONE ";
+		echo -e "Now ${ROMNIS}fy! your Device Tree! Press ${LCYAN}ENTER${NONE} when ${LPURP}Done.${NONE} ";
 		read NOOB;
-		echo "========================================================="
+		echo -e "${LPURP}=========================================================${NONE}"
 		echo -e '\n\n';
 		sleep 3;
 		echo -e '\n';
-		echo "I_IZ_NOOB :P";
+		echo -e "I_IZ_NOOB :P";
 
 		#Next ACTION to be Performed
 		echo -e '\n\n';
-		echo -e "=========================================================";
+		echo -e "${LRED}=========================================================${NONE}";
 		echo -e '\n';
-		echo -e " 1. Sync     ";
-		echo -e " 3. Build    ";
+		echo -e " ${CYAN}1${NONE}. ${RED}Sync${NONE}    ";
+		echo -e " ${CYAN}3${NONE}. ${GRN}Build${NONE}    ";
 		echo -e '\n';
 		echo -e " 4. EXIT     ";
-		echo -e "=========================================================";
+		echo -e "${LRED}=========================================================${NONE}";
 		read ACTION;
 		teh_action;
 
@@ -334,7 +337,7 @@ function build
 		if [[ "$KNWLOC" == y ]]; then
 			make_it;
 		else
-			echo -e "Do either of these two actions: \n1. Google it (Easier)\n2. Run this command in terminal : sgrep \"LOCAL_MODULE := Insert_MODULE_NAME_Here \".\n\n Press ENTER after it's done.";
+			echo -e "Do either of these two actions: \n1. ${BLU}G${NONE}${RED}o${NONE}${YELO}o${NONE}${BLU}g${NONE}${GRN}l${NONE}${RED}e${NONE} it (Easier)\n2. Run this command in terminal : ${LBLU}sgrep \"LOCAL_MODULE := Insert_MODULE_NAME_Here \"${NONE}.\n\n Press ${LCYAN}ENTER${NONE} after it's ${LPURP}Done.${NONE}.";
 			read OK;
 			make_it;
 		fi
@@ -342,9 +345,9 @@ function build
 
 	function make_it #Part of make_module
 	{
-		echo -e "Enter the Directory where the Module is made from";
+		echo -e "${LCYAN}ENTER${NONE} the Directory where the Module is made from";
 		read MODDIR;
-		echo -e "Do you want to push the Module to the Device ? (Running the Same ROM) [y/n]";
+		echo -e "Do you want to ${LRED}push the Module${NONE} to the ${LRED}Device${NONE} ? (Running the Same ROM) ${LGRN}[y/n]${NONE}";
 		read PMOD;
 		if [[ "$PMOD" == y ]]; then
 			mmmp -B $MODDIR;
@@ -353,18 +356,25 @@ function build
 		fi
 	} #make_it
 
+	echo -e "${LPURP}=========================================================${NONE}"
+	echo -e '\n';
 	echo -e "${CYAN}Initializing Build Environment${NONE}";
 	. build/envsetup.sh
-	echo -e "DONE."
+	echo -e "${LPURP}Done.${NONE}."
+	echo -e '\n';
 	echo -e "Select the Build Option:\n";
-	echo -e "1. Start Building ROM (ZIP output)"
-	echo -e "2. Clean only Staging Directories and Emulator Images (*.img)"
-	echo -e "3. Clean the Entire Build (/out) Directory"
-	echo -e "4. Make a Particular Module"
+	echo -e '\n';
+	echo -e "${GRN}1. Start Building ROM (ZIP output)${NONE}";
+	echo -e "${ORNG}2. Clean only Staging Directories and Emulator Images (*.img)${NONE}";
+	echo -e "${LRED}3. Clean the Entire Build (/out) Directory (THINK BEFORE SELECTING THIS!)${NONE}";
+	echo -e "${LGRN}4. Make a Particular Module${NONE}";
+	echo -e '\n';
+	echo -e "${LPURP}=========================================================${NONE}"
+	echo -e '\n';
 	read BOPT;
-
-	if [[ "$BOPT" == 1 ]]; then
-		echo -e "Should i use 'make' or 'mka' ?"
+	echo -e '\n';
+		if [[ "$BOPT" == 1 ]]; then
+		echo -e "Should i use '${YELO}make${NONE}' or '${RED}mka${NONE}' ?"
 		read MKWAY;
 		if [[ "$MKWAY" == make ]]; then
 			BCORES=$(grep -c ^processor /proc/cpuinfo);
@@ -397,13 +407,13 @@ function build
 
 	#Next ACTION to be Performed
 	echo -e '\n\n';
-	echo -e "=========================================================";
+	echo -e "${LRED}=========================================================${NONE}";
 	echo -e '\n';
-	echo -e " 1. Sync";
-	echo -e " 2. Pre-Build    ";
+	echo -e " ${RED}1. Sync${NONE}";
+	echo -e " ${YELO}2. Pre-Build${NONE} ";
 	echo -e '\n';
 	echo -e " 4. EXIT     ";
-	echo -e "=========================================================";
+	echo -e "${LRED}=========================================================${NONE}";
 	read ACTION;
 	teh_action;
 
@@ -412,7 +422,7 @@ function build
 function exitScriBt
 {
 	echo -e '\n\n';
-	echo -e "Thanks for using this ScriBt. Have a Nice Day";
+	echo -e "Thanks for using this ${LRED}S${NONE}cri${GRN}B${NONE}t. Have a Nice Day";
 	sleep 2;
 	exit 0;
 } #exitScriBt
