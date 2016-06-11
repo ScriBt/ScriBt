0f68e2526e3234a76089c35565587ee8961f3084
a7r3 <arvindultimate7352@gmail.com>
2016-06-12 00:16:26 +0530

    Add maven pkgs + Some crxn

--- ROM.sh
+++ ROM.sh
@@ -303,19 +303,20 @@ function installdeps
 	echo -e '\n';
 	echo -e "Checking and Installing Build Dependencies Now..."
 	echo -e '\n';
-	sudo apt-get install git-core gnupg ccache lzop flex bison \
-	gperf build-essential zip curl zlib1g-dev \
-	zlib1g-dev:i386 libc6-dev lib32ncurses5 lib32z1 \
-	lib32bz2-1.0 lib32ncurses5-dev x11proto-core-dev \
-	libx11-dev:i386 libreadline6-dev:i386 lib32z-dev \
-	libgl1-mesa-glx:i386 libgl1-mesa-dev g++-multilib \
-	mingw32 tofrodos python-markdown libxml2-utils xsltproc \
-	readline-common libreadline6-dev libreadline6 \
-	lib32readline-gplv2-dev libncurses5-dev lib32readline5 \
-	lib32readline6 libreadline-dev libreadline6-dev:i386 \
-	libreadline6:i386 bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev \
-	lib32bz2-dev libsdl1.2-dev libesd0-dev squashfs-tools \
-	pngcrush schedtool libwxgtk2.8-dev python liblz4-tool
+sudo apt-get install git-core gnupg ccache lzop flex bison \
+gperf build-essential zip curl zlib1g-dev \
+zlib1g-dev:i386 libc6-dev lib32ncurses5 lib32z1 \
+lib32bz2-1.0 lib32ncurses5-dev x11proto-core-dev \
+libx11-dev:i386 libreadline6-dev:i386 lib32z-dev \
+libgl1-mesa-glx:i386 libgl1-mesa-dev g++-multilib \
+mingw32 tofrodos python-markdown libxml2-utils xsltproc \
+readline-common libreadline6-dev libreadline6 \
+lib32readline-gplv2-dev libncurses5-dev lib32readline5 \
+lib32readline6 libreadline-dev libreadline6-dev:i386 \
+libreadline6:i386 bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev \
+lib32bz2-dev libsdl1.2-dev libesd0-dev squashfs-tools \
+pngcrush schedtool libwxgtk2.8-dev python liblz4-tool \
+maven maven2 
 	echo -e '\n';
 	echo -e "${RED}==========================================================${NONE}";
 	echo -e '\n\n'
