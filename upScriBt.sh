1f066ff99cb1228bcf25b8fa537ad823dd821fd5
a7r3 <arvindultimate7352@gmail.com>
2016-12-28 01:05:45 +0530

    Housekeeping 23.6 && Some moar ROMs
    ROM.rc && ROM.sh:
    * Add Some moar ROMs and their Vendor strats
            - Euphoria OS
            - F-AOSP
            - Parallax OS
            - Team Radium
    * Update GitHub URL for GZR Validus (Nougat)
    * Underp manifest repo for Validus Lollipop (TV-LP51)
    
    ROM.sh:
    * TARGET Var sets the target name
            Pure Nexus and Euphoria have a TARGET -> device-buildtype
            and other ROMs have a different TARGET -> rom_device-buildtype
    
    upScriBt.sh:
    * Add LVER representing Local VERSION
    * exit with status 1 if there's no .git folder
    	Chances are that .git is either manually deleted (after cloning), or downloaded from GitHub instead of cloning it
    	... which isn't recommended for upScriBt to work
    
    VERSION:
    * Bump eet to 1.52

--- upScriBt.sh
+++ upScriBt.sh
@@ -11,11 +11,19 @@ INF="\033[1;34m[!]\033[0m";
 SCS="\033[1;32m[!]\033[0m";
 QN="\033[1;31m[!]\033[0m";
 
+if [ ! -d .git ]; then # tell the user to re-clone ScriBt
+    echo -e "${FLD} Folder ${CL_WYT}.git${NONE} not found";
+    echo -e "${INF} Probably the folder's been deleted OR You've ${CL_WYT}Downloaded it${NONE}";
+    echo -e "${INF} ${CL_WYT}Re-clone${NONE} ScriBt for upScriBt to work properly\n";
+    exit 1;
+fi
+
 # Check Branch
 BRANCH="$(git rev-parse --abbrev-ref HEAD)";
 
 if [[ "$BRANCH" =~ (master|staging) ]]; then
-    RVER=`curl https://raw.githubusercontent.com/a7r3/ScriBt/${BRANCH}/VERSION -s`;
+    RVER=`curl https://raw.githubusercontent.com/a7r3/ScriBt/${BRANCH}/VERSION -s`; # Remote VERSION
+    LVER=`cat VERSION`; # Local VERSION
     LHEAD="$(git rev-parse HEAD)";
     git fetch -q origin ${BRANCH};
     RHEAD="$(git rev-parse origin/${BRANCH})";
